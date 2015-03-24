369248d51e47b7ac5b6a4642a112671e0c0b34c3
Konstantin <pilot911@yandex.ru>
2015-03-25 00:29:56 +0300

    Update installation

--- src/Telenok/Core/Support/Install/Controller.php
+++ src/Telenok/Core/Support/Install/Controller.php
@@ -261,7 +261,14 @@ class Controller {
 
 		foreach ($param as $k => $v)
 		{
-			$stub = preg_replace('/^[ \t]*(' . preg_quote($v) . '=)(.*)$/u', '$1' . $v, $stub);
+			if (preg_match('/^[ \t]*' . preg_quote($k, '/') . '=.*$/u', $stub))
+			{
+				$stub = preg_replace('/^[ \t]*(' . preg_quote($k, '/') . '=)(.*)$/u', '$1' . $v, $stub);
+			}
+			else
+			{
+				$stub .= '\n' . $k . '=' . $v;
+			}
 		}
 		
 		\File::put($path, $stub);
@@ -295,7 +302,14 @@ class Controller {
 
 		foreach ($param as $k => $v)
 		{
-			$stub = preg_replace('/^[ \t]*(' . preg_quote($v) . '=)(.*)$/u', '$1' . $v, $stub);
+			if (preg_match('/^[ \t]*' . preg_quote($k, '/') . '=.*$/u', $stub))
+			{
+				$stub = preg_replace('/^[ \t]*(' . preg_quote($k, '/') . '=)(.*)$/u', '$1' . $v, $stub);
+			}
+			else
+			{
+				$stub .= '\n' . $k . '=' . $v;
+			}
 		}
 
 		\File::put($path, $stub);
