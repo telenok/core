64364084ca624d68eb7309e643ff063babeafa14
pilot911 <support@telenok.com>
2016-02-03 08:06:21 +0300

    Update Controller.php

--- src/Telenok/Core/WidgetGroup/Standart/Controller.php
+++ src/Telenok/Core/WidgetGroup/Standart/Controller.php
@@ -1,8 +1,26 @@
 <?php namespace Telenok\Core\WidgetGroup\Standart;
 
+/**
+ * @class Telenok.Core.WidgetGroup.Standart.Controller
+ * Standart widget's group.
+ * @extends Telenok.Core.Interfaces.Widget.Group.Controller
+ */
 class Controller extends \Telenok\Core\Interfaces\Widget\Group\Controller {
 
+    /**
+     * @protected
+     * @property {String} $key
+     * Key for widget group.
+     * @member Telenok.Core.WidgetGroup.Standart.Controller
+     */
     protected $key = 'standart';
+    
+    /**
+     * @protected
+     * @property {String} $icon
+     * Icon for widget group.
+     * @member Telenok.Core.WidgetGroup.Standart.Controller
+     */
     protected $icon = 'fa fa-signal'; 
 
 }
