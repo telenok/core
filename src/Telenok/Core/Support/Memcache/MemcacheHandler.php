4131080a4f477026c333e5577b5b3ecfb17f0ee4
pilot911 <support@telenok.com>
2016-02-03 14:48:09 +0300

    Update MemcacheHandler.php

--- src/Telenok/Core/Support/Memcache/MemcacheHandler.php
+++ src/Telenok/Core/Support/Memcache/MemcacheHandler.php
@@ -2,14 +2,22 @@
 
 use Illuminate\Cache\Repository;
 
+/**
+ *  Create a new cache driven handler instance.
+ * 
+ * @class Telenok.Core.Support.Memcache.MemcacheHandler
+ * @uses Illuminate.Cache.Repository
+ * @uses SessionHandlerInterface
+ */
 class MemcacheHandler implements \SessionHandlerInterface {
 
     /**
+     * @constructor 
      * Create a new cache driven handler instance.
      *
-     * @param  \Illuminate\Cache\Repository  $cache
-     * @param  int  $minutes
-     * @return void
+     * @param  {Illuminate.Cache.Repository}  $cache
+     * @param  {Number} $minutes
+     * Can be float as part of minute.
      */
     public function __construct(Repository $cache, $minutes)
     {
@@ -17,40 +25,116 @@ class MemcacheHandler implements \SessionHandlerInterface {
         $this->minutes = $minutes;
     }
 
+    /**
+     * @method open
+     * Open session.
+     * See [php.net][1] for more.
+     * [1]: http://php.net/sessionhandlerinterface.open
+     * 
+     * @param {String} $savePath
+     * Save path.
+     * @param {String} $sessionName
+     * Session Name.
+     * 
+     * @return {Boolean}
+     * @throws {RuntimeException}
+     */
     public function open($savePath, $sessionName)
     {
         return true;
     }
 
+    /**
+     * @method close
+     * Open session.
+     * See [php.net][1] for more.
+     * [1]: http://php.net/sessionhandlerinterface.close
+     * 
+     * @return {Boolean}
+     */
     public function close()
     {
         return true;
     }
 
+    /**
+     * @method read
+     * Read session. 
+     * See [php.net][1] for more.
+     * [1]: http://php.net/sessionhandlerinterface.read
+     * @param {String} $sessionId
+     * @return {String}
+     * String as stored in persistent storage or empty string in all other cases.
+     * @throws {RuntimeException}
+     */
     public function read($sessionId)
     {
         return $this->cache->get($sessionId) ? : '';
     }
 
+    /**
+     * @method write
+     * Commit session to storage.
+     * See [php.net][1] for more.
+     * [1]: http://php.net/sessionhandlerinterface.read
+     * @param {String} $sessionId
+     * Session ID.
+     * @param {String} $data
+     * Session serialized data to save.
+     * @return {Boolean}
+     * @throws {RuntimeException}
+     */
     public function write($sessionId, $data)
     {
         return $this->cache->put($sessionId, $data, $this->minutes);
     }
 
+    /**
+     * @method destroy
+     * Commit session to storage.
+     * See [php.net][1] for more.
+     * [1]: http://php.net/sessionhandlerinterface.read
+     * @param {String} $sessionId
+     * Session ID.
+     * @return {Boolean}
+     * @throws {RuntimeException}
+     */
     public function destroy($sessionId)
     {
         return $this->cache->forget($sessionId);
     }
-
+    /**
+     * Garbage collection for storage.
+     *
+     * @see http://php.net/sessionhandlerinterface.gc
+     *
+     * @param integer $lifetime Max lifetime in seconds to keep sessions stored.
+     *
+     * @throws \RuntimeException On fatal error.
+     *
+     * @return boolean
+     */
+     
+    /**
+     * @method gc
+     * Garbage collection for storage.
+     * See [php.net][1] for more.
+     * [1]: http://php.net/sessionhandlerinterface.gc
+     * @param {Integer} $lifetime
+     * Max lifetime in seconds to keep sessions stored.
+     * @return {Boolean}
+     * @throws {RuntimeException}
+     */
     public function gc($lifetime)
     {
         return true;
     }
 
     /**
+     * @method getCache
      * Get the underlying cache repository.
      *
-     * @return \Illuminate\Cache\Repository
+     * @return {Illuminate.Cache.Repository}
      */
     public function getCache()
     {
