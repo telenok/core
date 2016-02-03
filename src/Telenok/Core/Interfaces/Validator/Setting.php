dae66fbe54adee7d97e82873e317808bbc09eec7
pilot911 <support@telenok.com>
2016-02-03 10:54:20 +0300

    Update Setting.php

--- src/Telenok/Core/Interfaces/Validator/Setting.php
+++ src/Telenok/Core/Interfaces/Validator/Setting.php
@@ -1,25 +1,83 @@
 <?php namespace Telenok\Core\Interfaces\Validator;
 
+/**
+ * @class Telenok.Core.Interfaces.Validator.Setting
+ * Validator for Telenok's setting.
+ */
 class Setting {
 
+    /**
+     * @protected
+     * @property {Array} $ruleList
+     * List of validation's rules.
+     * @member Telenok.Core.Interfaces.Validator.Setting
+     */
     protected $ruleList = [];
+    
+    /**
+     * @protected
+     * @property {Illuminate.Support.Collection} $input
+     * Input collection to validate.
+     * @member Telenok.Core.Interfaces.Validator.Setting
+     */
     protected $input = [];
+    
+    /**
+     * @protected
+     * @property {Telenok.Core.Interfaces.Validator.Validator} $validator
+     * Validator which make validation.
+     * @member Telenok.Core.Interfaces.Validator.Setting
+     */
     protected $validator;
+    
+    /**
+     * @protected
+     * @property {Array} $message
+     * List of custom messages.
+     * @member Telenok.Core.Interfaces.Validator.Setting
+     */
     protected $message = [];
+    
+    /**
+     * @protected
+     * @property {Array} $customAttribute
+     * Custom notification attributes.
+     * @member Telenok.Core.Interfaces.Validator.Setting
+     */
     protected $customAttribute = [];
     
+    /**
+     * @method setInput
+     * Set input.
+     * @param {Array} $param
+     * @return {Telenok.Core.Interfaces.Validator.Setting}
+     * @member Telenok.Core.Interfaces.Validator.Setting
+     */
     public function setInput($param = [])
     {
         $this->input = collect($param);
 
         return $this;
     }
-    
+
+    /**
+     * @method getInput
+     * Return input collection.
+     * @return {Illuminate.Support.Collection}
+     * @member Telenok.Core.Interfaces.Validator.Setting
+     */
     public function getInput()
     {
         return $this->input;
     }
     
+    /**
+     * @method setMessage
+     * Set messages.
+     * @param {Array} $param
+     * @return {Telenok.Core.Interfaces.Validator.Setting}
+     * @member Telenok.Core.Interfaces.Validator.Setting
+     */
     public function setMessage($param = [])
     {
         $this->message = array_merge(trans('core::default.error'), (array)$param);
@@ -27,11 +85,24 @@ class Setting {
         return $this;
     }
     
+    /**
+     * @method getMessage
+     * Return messages.
+     * @return {Array}
+     * @member Telenok.Core.Interfaces.Validator.Setting
+     */
     public function getMessage()
     {
         return $this->message;
     }
     
+    /**
+     * @method setRuleList
+     * Set rule's list.
+     * @param {Array} $param
+     * @return {Telenok.Core.Interfaces.Validator.Setting}
+     * @member Telenok.Core.Interfaces.Validator.Setting
+     */
     public function setRuleList($param = [])
     {
         $this->ruleList = $this->processRule($param);
@@ -39,6 +110,12 @@ class Setting {
         return $this;
     }
     
+    /**
+     * @method getRuleList
+     * Return rule's list.
+     * @return {Array}
+     * @member Telenok.Core.Interfaces.Validator.Setting
+     */
     public function getRuleList()
     {
         if (empty($this->ruleList))
@@ -49,6 +126,13 @@ class Setting {
         return $this->ruleList;
     } 
     
+    /**
+     * @method setCustomAttribute
+     * Set list of custom attributes for better notificate user about error.
+     * @param {Array} $param
+     * @return {Telenok.Core.Interfaces.Validator.Setting}
+     * @member Telenok.Core.Interfaces.Validator.Setting
+     */
     public function setCustomAttribute($param = [])
     {
         $this->customAttribute = $param;
@@ -56,11 +140,24 @@ class Setting {
         return $this;
     }
     
+    /**
+     * @method getCustomAttribute
+     * Return custom attribute list.
+     * @return {Array}
+     * @member Telenok.Core.Interfaces.Validator.Setting
+     */
     public function getCustomAttribute()
     {
         return $this->customAttribute;
     }
 
+    /**
+     * @method processRule
+     * Process rule for converting them to one-level array.
+     * @param {Array} $rule
+     * @return {Array}
+     * @member Telenok.Core.Interfaces.Validator.Setting
+     */
     protected function processRule($rule)
     {
         array_walk_recursive($rule, function(&$el, $key, $this_) {
@@ -70,6 +167,12 @@ class Setting {
         return $rule;
     }
 
+    /**
+     * @method passes
+     * Start validation process.
+     * @return {Boolean}
+     * @member Telenok.Core.Interfaces.Validator.Setting
+     */
     public function passes()
     {
         $this->validator = app('validator')->make($this->getInput()->toArray(), $this->getRuleList(), $this->getMessage());
