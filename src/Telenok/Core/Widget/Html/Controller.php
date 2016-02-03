a6850bba10fefd60abb95a3112c4d53792bb53a4
pilot911 <support@telenok.com>
2016-02-03 13:25:19 +0300

    Update Controller.php

--- src/Telenok/Core/Widget/Html/Controller.php
+++ src/Telenok/Core/Widget/Html/Controller.php
@@ -1,10 +1,35 @@
 <?php namespace Telenok\Core\Widget\Html;
 
+/**
+ * @class Telenok.Core.Widget.Html.Controller
+ * Class presents html widget.
+ * 
+ * @extends Telenok.Core.Interfaces.Widget.Controller
+ */
 class Controller extends \App\Telenok\Core\Interfaces\Widget\Controller {
 
+    /**
+     * @protected
+     * @property {String} $key
+     * Key of widget.
+     * @member Telenok.Core.Widget.Html.Controller
+     */
     protected $key = 'html';
+    
+    /**
+     * @protected
+     * @property {String} $parent
+     * Parent's widget key.
+     * @member Telenok.Core.Widget.Html.Controller
+     */
     protected $parent = 'standart';
 
+    /**
+     * @method getNotCachedContent
+     * Return not cached content of widget.
+     * @return {String}
+     * @member Telenok.Core.Widget.Html.Controller
+     */
 	public function getNotCachedContent()
 	{
         if ($t = $this->getFileTemplatePath())
