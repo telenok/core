565fe51b8b345ea694cc2fb03061a1d090b9d5b4
pilot911 <support@telenok.com>
2016-02-03 13:08:26 +0300

    Update Controller.php

--- src/Telenok/Core/Widget/Php/Controller.php
+++ src/Telenok/Core/Widget/Php/Controller.php
@@ -1,8 +1,26 @@
 <?php namespace Telenok\Core\Widget\Php;
 
+/**
+ * @class Telenok.Core.Widget.Php.Controller
+ * Class presents php execution widget.
+ * 
+ * @extends Telenok.Core.Interfaces.Widget.Controller
+ */
 class Controller extends \App\Telenok\Core\Interfaces\Widget\Controller {
 
+    /**
+     * @protected
+     * @property {String} $key
+     * Key of widget.
+     * @member Telenok.Core.Widget.Php.Controller
+     */
     protected $key = 'php';
+    
+    /**
+     * @protected
+     * @property {String} $parent
+     * Parent's widget key.
+     * @member Telenok.Core.Widget.Php.Controller
+     */
     protected $parent = 'standart';
-
 }
