46ae7559b71f591561f387feb766f277ac8f3f9e
pilot911 <support@telenok.com>
2016-02-04 08:28:58 +0300

    Update PackageInfo.php

--- src/Telenok/Core/Interfaces/Support/PackageInfo.php
+++ src/Telenok/Core/Interfaces/Support/PackageInfo.php
@@ -10,14 +10,68 @@ abstract class PackageInfo {
 
     use \Telenok\Core\Support\Traits\Language;
 
+    /**
+     * @protected
+     * @property {String} $key
+     * Package's key.
+     * @member Telenok.Core.Interfaces.Support.PackageInfo
+     */	
 	protected $key;
+	
+    /**
+     * @protected
+     * @property {String} $baseClass
+     * Package's base class.
+     * @member Telenok.Core.Interfaces.Support.PackageInfo
+     */	
 	protected $baseClass;
+	
+    /**
+     * @protected
+     * @property {String} $versionPackage
+     * Package's version.
+     * @member Telenok.Core.Interfaces.Support.PackageInfo
+     */	
 	protected $versionPackage;
+	
+    /**
+     * @protected
+     * @property {String} $versionTelenok
+     * Maximum version of Telenok CMS.
+     * @member Telenok.Core.Interfaces.Support.PackageInfo
+     */	
 	protected $versionTelenok;
+	
+    /**
+     * @protected
+     * @property {String} $title
+     * Title of package.
+     * @member Telenok.Core.Interfaces.Support.PackageInfo
+     */	
 	protected $title;
+	
+    /**
+     * @protected
+     * @property {String} $description
+     * Description of package.
+     * @member Telenok.Core.Interfaces.Support.PackageInfo
+     */	
 	protected $description;
+	
+    /**
+     * @protected
+     * @property {String} $image
+     * Class of image for package.
+     * @member Telenok.Core.Interfaces.Support.PackageInfo
+     */	
 	protected $image;
 	
+    /**
+     * @protected
+     * @property {String} $languageDirectory
+     * Language directory for widgets.
+     * @member Telenok.Core.Interfaces.Widget.Group.Controller
+     */
 	public function setKey($param = '')
 	{
 		$this->key = $param;
@@ -25,6 +79,12 @@ abstract class PackageInfo {
 		return $this;
 	}
 
+    /**
+     * @protected
+     * @property {String} $languageDirectory
+     * Language directory for widgets.
+     * @member Telenok.Core.Interfaces.Widget.Group.Controller
+     */
 	public function getKey()
 	{
 		return $this->key;
@@ -37,11 +97,23 @@ abstract class PackageInfo {
 		return $this;
 	}
 
+    /**
+     * @protected
+     * @property {String} $languageDirectory
+     * Language directory for widgets.
+     * @member Telenok.Core.Interfaces.Widget.Group.Controller
+     */
 	public function getTitle()
 	{
 		return $this->title ?: $this->LL('title');
 	}
 
+    /**
+     * @protected
+     * @property {String} $languageDirectory
+     * Language directory for widgets.
+     * @member Telenok.Core.Interfaces.Widget.Group.Controller
+     */
 	public function setDescription($param = '')
 	{
 		$this->description = $param;
@@ -49,11 +121,23 @@ abstract class PackageInfo {
 		return $this;
 	}
 
+    /**
+     * @protected
+     * @property {String} $languageDirectory
+     * Language directory for widgets.
+     * @member Telenok.Core.Interfaces.Widget.Group.Controller
+     */
 	public function getDescription()
 	{
 		return $this->description ?: $this->LL("description");
 	}
 
+    /**
+     * @protected
+     * @property {String} $languageDirectory
+     * Language directory for widgets.
+     * @member Telenok.Core.Interfaces.Widget.Group.Controller
+     */
 	public function setImage($param = '')
 	{
 		$this->image = $param;
@@ -61,16 +145,34 @@ abstract class PackageInfo {
 		return $this;
 	}
 
+    /**
+     * @protected
+     * @property {String} $languageDirectory
+     * Language directory for widgets.
+     * @member Telenok.Core.Interfaces.Widget.Group.Controller
+     */
 	public function getImage()
 	{
 		return $this->image;
 	}
-	
+
+    /**
+     * @protected
+     * @property {String} $languageDirectory
+     * Language directory for widgets.
+     * @member Telenok.Core.Interfaces.Widget.Group.Controller
+     */
 	public function getBaseClass()
 	{
 		return $this->baseClass;
 	}
-	
+
+    /**
+     * @protected
+     * @property {String} $languageDirectory
+     * Language directory for widgets.
+     * @member Telenok.Core.Interfaces.Widget.Group.Controller
+     */
 	public function setBaseClass($param)
 	{
 		$this->baseClass = $param;
