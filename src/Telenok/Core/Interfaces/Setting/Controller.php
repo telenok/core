863eaed5e5b94013cf49a44bbed2f2aed867de6b
pilot911 <support@telenok.com>
2016-02-04 08:51:36 +0300

    Update Controller.php

--- src/Telenok/Core/Interfaces/Setting/Controller.php
+++ src/Telenok/Core/Interfaces/Setting/Controller.php
@@ -1,14 +1,54 @@
-<?php
+<?php namespace Telenok\Core\Interfaces\Setting;
 
-namespace Telenok\Core\Interfaces\Setting;
-
+/**
+ * @class Telenok.Core.Interfaces.Setting.Controller
+ * Base controller for setting.
+ * 
+ * @extends Telenok.Core.Interfaces.Controller.Controller
+ */
 class Controller extends \Telenok\Core\Interfaces\Controller\Controller {
 
+    /**
+     * @protected
+     * @property {Array} $defaultValue
+     * Default setting values.
+     * @member Telenok.Core.Interfaces.Setting.Controller
+     */	
     protected $defaultValue = [];
+    
+    /**
+     * @protected
+     * @property {Array} $ruleList
+     * List of rules to validate setting before saving.
+     * @member Telenok.Core.Interfaces.Setting.Controller
+     */	
     protected $ruleList = [];
+    
+    /**
+     * @protected
+     * @property {String} $formSettingContentView
+     * Name of view for display settings in form.
+     * @member Telenok.Core.Interfaces.Setting.Controller
+     */	
     protected $formSettingContentView = '';
+    
+    /**
+     * @protected
+     * @property {String} $languageDirectory
+     * Relative path to language directory of settings.
+     * @member Telenok.Core.Interfaces.Setting.Controller
+     */	
     protected $languageDirectory = 'setting';
 
+    /**
+     * @method getFormSettingContent
+     * Set group widget's model.
+     * @param {Telenok.Core.Interfaces.Eloquent.Object.Model} $field
+     * @param {Telenok.Core.Interfaces.Eloquent.Object.Model} $model
+     * @param {String} $uniqueId
+     * @return {String}
+     * @member Telenok.Core.Interfaces.Setting.Controller
+     */
     public function getFormSettingContent($field, $model, $uniqueId)
     {
         return view($this->getFormSettingContentView(), [
@@ -19,11 +59,24 @@ class Controller extends \Telenok\Core\Interfaces\Controller\Controller {
                 ])->render();
     }
 
+    /**
+     * @method getFormSettingContentView
+     * Return name of view for display settings in form.
+     * @return {String}
+     * @member Telenok.Core.Interfaces.Setting.Controller
+     */
     public function getFormSettingContentView()
     {
         return $this->formSettingContentView ? : "{$this->getPackage()}::setting/{$this->getKey()}.content";
     }
 
+    /**
+     * @method validate
+     * Validate input data.
+     * @param {Array} $input
+     * @return {Boolean}
+     * @member Telenok.Core.Interfaces.Setting.Controller
+     */
     public function validate($input = [])
     {
         $validator = $this->validator($this->ruleList, $input);
@@ -33,7 +86,17 @@ class Controller extends \Telenok\Core\Interfaces\Controller\Controller {
             throw $this->validateException()->setMessageError($validator->messages());
         }
     }
-
+    
+    /**
+     * @method validator
+     * Create and return validator.
+     * @param {Array} $rule
+     * @param {Array} $input
+     * @param {Array} $message
+     * @param {Array} $customAttribute
+     * @return {Telenok.Core.Interfaces.Validator\Setting}
+     * @member Telenok.Core.Interfaces.Setting.Controller
+     */
     public function validator($rule = [], $input = [], $message = [], $customAttribute = [])
     {
         return app('\Telenok\Core\Interfaces\Validator\Setting')
@@ -43,11 +106,25 @@ class Controller extends \Telenok\Core\Interfaces\Controller\Controller {
                         ->setCustomAttribute($customAttribute);
     }
 
+    /**
+     * @method validateException
+     * Create and return validator's exception.
+     * @return {Telenok.Core.Support.Exception.Validator}
+     * @member Telenok.Core.Interfaces.Setting.Controller
+     */
     public function validateException()
     {
         return new \Telenok\Core\Support\Exception\Validator;
     }
 
+    /**
+     * @method fillSettingValue
+     * Set setting's values in global app('config').
+     * @param {Telenok.Core.Interfaces.Eloquent.Object.Model} $model
+     * @param {mixed} $value
+     * @return {void}
+     * @member Telenok.Core.Interfaces.Setting.Controller
+     */
     public function fillSettingValue($model, $value)
     {
         collect($value)->each(function($item, $key)
@@ -61,6 +138,14 @@ class Controller extends \Telenok\Core\Interfaces\Controller\Controller {
         });
     }
 
+    /**
+     * @method save
+     * Save setting's values in database.
+     * @param {Telenok.Core.Interfaces.Eloquent.Object.Model} $model
+     * @param {Illuminte.Support.Collection} $input
+     * @return {Telenok.Core.Interfaces.Eloquent.Object.Model}
+     * @member Telenok.Core.Interfaces.Setting.Controller
+     */
     public function save($model, $input)
     {
         if ($this->defaultValue)
