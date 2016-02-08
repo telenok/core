a56662b4b3e45e67fb5f8104dac69d0ec092a77b
Konstantin <pilot911@yandex.ru>
2016-02-08 22:32:09 +0300

    Last updates

--- src/Telenok/Core/Support/Config/Theme.php
+++ src/Telenok/Core/Support/Config/Theme.php
@@ -6,23 +6,8 @@
  */
 class Theme {
 
-    public static function view($view = null, $data = [], $mergeData = [])
+    public static function chooseView()
     {
-        if ($view)
-        {
-            $theme = config('telenok.view.theme');
-            
-            if (strpos($view, '::') !== FALSE && view()->exists($v = str_replace('::', '::' . $theme, $view)))
-            {
-                return view($v, $data, $mergeData);
-            }
-            else if (view()->exists($v = $theme . '.' . $view))
-            {
-                return view($v, $data, $mergeData);
-            }
-        }
-
-        return view($view, $data, $mergeData);
     }
     
     public static function view($view = null, $data = [], $mergeData = [])
