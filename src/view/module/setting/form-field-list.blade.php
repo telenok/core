7760e01910396b32e285d7ac9c4b2e5bb15f7cd0
Konstantin <pilot911@yandex.ru>
2016-03-08 04:27:43 +0300

    Last updates

--- src/view/module/setting/form-field-list.blade.php
+++ src/view/module/setting/form-field-list.blade.php
@@ -1,22 +1,22 @@
 	@if (!in_array($field->code, ['value'], true))
 
-		{!! app('telenok.config.repository')->getObjectFieldController($field->key)->getFormModelContent($controller, $model, $field, $uniqueId) !!}
+            {!! app('telenok.config.repository')->getObjectFieldController($field->key)->getFormModelContent($controller, $model, $field, $uniqueId) !!}
 
 	@elseif ($field->code=='value')
 
-		<?php
+            <?php
 
-		try
-		{
-                    $w = app('telenok.config.repository')->getSetting(strtolower($model->code));
-			
-                    echo $w->getFormSettingContent($field, $model, $uniqueId);
-		}
-		catch (\Exception $e)
-		{
-                    echo app('telenok.config.repository')->getObjectFieldController($field->key)->getFormModelContent($controller, $model, $field, $uniqueId);
-		}
-		
-		?>
+            try
+            {
+                $w = app('telenok.config.repository')->getSetting(strtolower($model->code));
+
+                echo $w->getFormSettingContent($field, $model, $uniqueId);
+            }
+            catch (\Exception $e)
+            {
+                echo app('telenok.config.repository')->getObjectFieldController($field->key)->getFormModelContent($controller, $model, $field, $uniqueId);
+            }
+
+            ?>
 
 	@endif
